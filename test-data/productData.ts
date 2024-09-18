22ad18dd8d88fae9b383f96cdba5ecd428bf002c
Sara (Risclover) <sara091592@gmail.com>
2024-09-18 08:27:43 -0700

    Create test-data files

---
+++ test-data/productData.ts
@@ -0,0 +1,11 @@
+export const productCategories = {
+  selectedCategory: "Hammer",
+  secondCategory: "Wrench",
+  noResultsCategory: "Grinder",
+  multiplePagesCategory: "Measures",
+};
+
+export const brandCategories = {
+  selectedBrand: "ForgeFlex Tools",
+  secondBrand: "MightyCraft Hardware",
+};
